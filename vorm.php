7d95694d6b82c7857232b5fdea7f1a545bc4a460
tailohallap <hallaptailo@gmail.com>
2016-03-02 22:20:12 +0200

    Update vorm.php

--- vorm.php
+++ vorm.php
@@ -7,8 +7,8 @@
       <br>
       <h1>-_-</h1>
     </div>
-    
-          <center><input type="text" value=""name="kasutaja" placeholder="Kirjuta nimi"><br>
+          <center><input type="text" value="" name="kasutaja" id="kasutaja" placeholder="Kirjuta nimi"><br>
+		  <input type="hidden" id="kontroll" name="kontroll" value=0>
           <button class="OK" type="submit" title="OK"><em>Lisa tabelisse</em></button></center><br>
       </form>
     </div>
