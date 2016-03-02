e1677d873bd8f37b287636fd1a40a3276977be33
tailohallap <hallaptailo@gmail.com>
2016-03-02 22:17:35 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,29 @@
-<?php
-require("vorm.php");
-?>
-
-<?php
-	
-	$items = json_decode(file_get_contents("data.txt"), 2);
-	if(is_array($items)) {
-		foreach($items as $key => $item) {
-	?>
-			<table><tr>
-				<td><a href="one.php?key=<?=$key?>"><?=$item["kasutaja"]?></a></td>
-        
-			</tr>
-	</table>
-	<?php
-		}
-	}
-	?>
-
-
-
+<?php 
+require("vorm.php"); 
+?> 
+<table align="left" cellpadding="5" rowspan="5"> 
+<tr> 
+         <th>Kasutaja</th> 
+         <th>Vaata</th> 
+ 	 <th>Muuda</th> 
+ 	 <th>Eemalda</th> 
+</tr> 
+<?php 
+require_once("api.php"); 
+$items = kasutaja_API(); 
+ 	 
+foreach ((array) $items as $key => $item) { 
+?> 
+<tr> 
+	<td><?=$item["kasutaja"]?></td> 
+	<td><a href="one.php?key=<?=$key?>"><?="Vaata"?></a></td> 
+	<td><a href="muuda.php?key=<?=$key?>"><?="Muuda"?></a></td> 
+	<td><a href="kustuta.php?key=<?=$key?>"><?="Eemalda"?></a></td> 
+</tr> 
+<?php 
+} 
+?> 
+</table> 
 
 
 
