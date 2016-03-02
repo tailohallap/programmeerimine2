5c5eeecc199210982dad54cfc2e54bf20eb47381
tailohallap <hallaptailo@gmail.com>
2016-03-02 22:19:12 +0200

    Update saada.php

--- saada.php
+++ saada.php
@@ -1,15 +1,48 @@
 <?php
-$kasutaja = $_POST['kasutaja'];
-
-
-$data = json_decode(file_get_contents("data.txt"), 2);
-
-$data[] = array("kasutaja" => $kasutaja);
-
-file_put_contents("data.txt", json_encode($data));
-
-header("Location: index.php");
-
+require_once("api.php");
+$kontroll = $_POST["kontroll"];
+if ($kontroll == 0){
+	$item = array(
+	"id" => $_POST["id"],
+	"kasutaja" => $_POST["kasutaja"],
+	"kontroll" => $_POST["kontroll"]);
+	
+	salvesta_API($item);
+	}
+else {
+	$item = array(
+	"id" => $_POST["id"],
+	"kasutaja" => $_POST["kasutaja"],
+	"kontroll" => $_POST["kontroll"]);
+	
+	muuda_API($item);
+	}
 ?>
-
-
+<?php
+if ($kontroll == 0){?>
+<div class="container">
+  <form role="form" action="index.php" method="get"><br><br>
+  
+  <p><b>Lisatud!</b></p>
+  
+  <button type="submit" class="btn btn-default">Tagasi</button>
+  </form>
+</div>
+</body>
+</html>
+<?php
+}
+else {?>
+<div class="container">
+  <form role="form" action="index.php" method="get"><br><br>
+  
+  <p><b>Muudetud!</b></p>
+  
+  <button type="submit" class="btn btn-default">Tagasi</button>
+  </form>
+</div>
+</body>
+</html>
+<?php
+}
+?>
