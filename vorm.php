8be83b61eef193026136ab84ac5e893b2897cf4e
tailohallap <hallaptailo@gmail.com>
2016-03-12 12:31:05 +0200

    Update vorm.php

--- vorm.php
+++ vorm.php
@@ -2,18 +2,22 @@
 
 <html>
 <head>
+<title>Vorm</title>
+</head>
+<body>
+
 <form action="saada.php" method="POST" align="center">
 <link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
-      <br>
-      <h1>-_-</h1>
-    </div>
-          <center><input type="text" value="" name="kasutaja" id="kasutaja" placeholder="Kirjuta nimi"><br>
-		  <input type="hidden" id="kontroll" name="kontroll" value=0>
-          <button class="OK" type="submit" title="OK"><em>Lisa tabelisse</em></button></center><br>
-      </form>
-    </div>
-    
-  </header>
+<br>
+<h3>Vorm</h3>
+</div>
+<center><input type="text" value="" name="kasutaja" id="kasutaja" placeholder="Kirjuta nimi"><br>
+<input type="hidden" id="kontroll" name="kontroll" value=0>
+<button class="OK" type="submit" title="OK"><em>Lisa tabelisse</em></button></center><br>
+
+</form>
+</div>
+</header>
 </div>
 
 </body>
