1a2e88a88928d37f5804738f340a1c148fb5d1bf
tailohallap <hallaptailo@gmail.com>
2016-03-23 12:37:01 +0200

    Update muuda.php

--- muuda.php
+++ muuda.php
@@ -1,24 +1,16 @@
 <?php
-require_once("api.php");
-$item = vaata_API($_GET["key"]);
+	require_once("api.php");
+	$item = vaata_API($_GET["key"]);
 ?>
 <!DOCTYPE html>
-
 <html>
-<head>
-<form action="saada.php" method="POST" align="center">
-<link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
-      <br>
-      <h1>-_-</h1>
-    </div>
-          <center><input type="text" value="<?=$item["kasutaja"]?>" name="kasutaja" id="kasutaja"><br>
-		  <input type="hidden" id="kontroll" name="kontroll" value=0>
-          <button class="OK" type="submit" title="OK"><em>Muuda</em></button></center><br>
-      </form>
-    </div>
-    
-  </header>
+<body>
+	<form action="saada.php" method="POST" align="center">
+	<p><center><input type="text" value="<?=$item["kasutaja"]?>" name="kasutaja" id="kasutaja"></center></p>
+	<input type="hidden" id="tuvasta" name="tuvasta" value=0>
+	<p><center><button class="OK" type="submit" title="OK"><em>Muuda</em></button></center></p>
+</form>
 </div>
-
+</div>
 </body>
 </html>
