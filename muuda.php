e94f1ea129455c047ed6f823b2bf80217c6a6739
tailohallap <hallaptailo@gmail.com>
2016-03-23 12:37:17 +0200

    Update muuda.php

--- muuda.php
+++ muuda.php
@@ -6,11 +6,9 @@
 <html>
 <body>
 	<form action="saada.php" method="POST" align="center">
-	<p><center><input type="text" value="<?=$item["kasutaja"]?>" name="kasutaja" id="kasutaja"></center></p>
-	<input type="hidden" id="tuvasta" name="tuvasta" value=0>
-	<p><center><button class="OK" type="submit" title="OK"><em>Muuda</em></button></center></p>
-</form>
-</div>
-</div>
+		<p><center><input type="text" value="<?=$item["kasutaja"]?>" name="kasutaja" id="kasutaja"></center></p>
+		<input type="hidden" id="tuvasta" name="tuvasta" value=0>
+		<p><center><button class="OK" type="submit" title="OK"><em>Muuda</em></button></center></p>
+	</form>
 </body>
 </html>
