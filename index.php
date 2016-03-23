dc0d4b09120695371481c2a14882ad20b71adb53
tailohallap <hallaptailo@gmail.com>
2016-03-23 12:40:47 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,33 +3,33 @@
 ?>
 
 <html>
-<body>
-<table align="left" cellpadding="5" rowspan="5"> 
-<tr> 
-         <th>Kasutaja</th> 
-         <th>Vaata</th> 
- 	 <th>Muuda</th> 
- 	 <th>Eemalda</th> 
-</tr> 
+	<body>
+		<table align="left" cellpadding="5" rowspan="5"> 
+			<tr> 
+        			 <th>Kasutaja</th> 
+        			 <th>Vaata</th> 
+ 	 			<th>Muuda</th> 
+ 	 			<th>Eemalda</th> 
+			</tr> 
 
-<?php 
-	require_once("api.php"); 
-	$items = kasutaja_API(); 
+		<?php 
+			require_once("api.php"); 
+			$items = kasutaja_API(); 
  	 
-	foreach ((array) $items as $key => $item) { 
-?> 
+			foreach ((array) $items as $key => $item) { 
+		?> 
 
-<tr> 
-	<td><?=$item["kasutaja"]?></td> 
-	<td><a href="one.php?key=<?=$key?>"><?="Vaata"?></a></td> 
-	<td><a href="muuda.php?key=<?=$key?>"><?="Muuda"?></a></td> 
-	<td><a href="kustuta.php?key=<?=$key?>"><?="Eemalda"?></a></td> 
-</tr> 
+			<tr> 
+				<td><?=$item["kasutaja"]?></td> 
+				<td><a href="one.php?key=<?=$key?>"><?="Vaata"?></a></td> 
+				<td><a href="muuda.php?key=<?=$key?>"><?="Muuda"?></a></td> 
+				<td><a href="kustuta.php?key=<?=$key?>"><?="Eemalda"?></a></td> 
+			</tr> 
 
-<?php 
-} 
-?> 
+		<?php 
+		} 
+		?> 
 
-</table>
-</body>
+		</table>
+	</body>
 </html>
