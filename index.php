ee6618b1d158feed2570551bff9314b4b0d3a128
tailohallap <hallaptailo@gmail.com>
2016-03-23 12:39:34 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php 
-require("vorm.php"); 
-?> 
+	require("vorm.php"); 
+?>
+
+<html>
+<body>
 <table align="left" cellpadding="5" rowspan="5"> 
 <tr> 
          <th>Kasutaja</th> 
@@ -8,25 +11,25 @@ require("vorm.php");
  	 <th>Muuda</th> 
  	 <th>Eemalda</th> 
 </tr> 
+
 <?php 
-require_once("api.php"); 
-$items = kasutaja_API(); 
+	require_once("api.php"); 
+	$items = kasutaja_API(); 
  	 
-foreach ((array) $items as $key => $item) { 
+	foreach ((array) $items as $key => $item) { 
 ?> 
+
 <tr> 
 	<td><?=$item["kasutaja"]?></td> 
 	<td><a href="one.php?key=<?=$key?>"><?="Vaata"?></a></td> 
 	<td><a href="muuda.php?key=<?=$key?>"><?="Muuda"?></a></td> 
 	<td><a href="kustuta.php?key=<?=$key?>"><?="Eemalda"?></a></td> 
 </tr> 
+
 <?php 
 } 
 ?> 
-</table> 
 
-
-
-
-
-
+</table>
+</body>
+</html>
